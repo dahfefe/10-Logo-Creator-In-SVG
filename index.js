753943884ffa6a95cea33a560c489ada05b29b3b
Felix <157293782+dahfefe@users.noreply.github.com>
2024-04-13 02:49:37 -0700

    added user input code in SVG generation code

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const generateLogo =
 `
 <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-  <circle cx="150" cy="100" r="80" fill="green" />
+  <${shape} cx="150" cy="100" r="80" fill="${shapeColor}" />
 
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
 
 </svg>
   
