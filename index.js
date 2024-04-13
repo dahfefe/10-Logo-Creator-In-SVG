1951ea1b63c05748fe75979c5a4b641d95410e84
Felix <157293782+dahfefe@users.noreply.github.com>
2024-04-13 02:53:29 -0700

    update writeFile function and .then to resolve promises

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const generateLogo =
 `
 ;
 
-// inquirer prompts needed for obtaining user response per section of README.md file
+// inquirer prompts needed for obtaining user response 
 inquirer
   .prompt([
     {
@@ -54,41 +54,10 @@ inquirer
     const logoInputs = generateLogo(responses);
 
     // writeFile function generates README file from user responses and error logging to aid in troubleshooting
-    fs.writeFile('README_auto.md', readMeContent, (err) =>
+    fs.writeFile('logo.svg', logoInputs, (err) =>
       err ? console.log(err) : console.log("Generated logo.svg")
     );
-  });
-
-  // function to render correspoding badge to README file based on user selection
-  function renderLicenseBadge(license) {
-    const licenseBadges = {
-      "MIT License": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-      "Apache License 2.0": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-      "GNU General Public License v3.0": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-      "Mozilla Public License 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
-    };
-
-    if (licenseBadges[license]) {
-      return licenseBadges[license];
-    } else {
-      return ``;
-    }
-  }
-
-  // function to render correspoding license link to README file based on user selection
-  function renderLicenseLink(license) {
-    const licenseLinks = {
-      "MIT License": "https://opensource.org/license/mit",
-      "Apache License 2.0": "http://www.apache.org/licenses/LICENSE-2.0",
-      "GNU General Public License v3.0": "https://www.gnu.org/licenses/gpl-3.0.en.html",
-      "Mozilla Public License 2.0": "https://www.mozilla.org/en-US/MPL/2.0/",
-    };
-
-    if (licenseLinks[license]) {
-      return licenseLinks[license];
-    } else {
-      return ``;
-    }
+});
   
-  }
+
   
